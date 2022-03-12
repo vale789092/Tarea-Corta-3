8f8eff5f33052660345c4f0bc81f00fa7dc322de
vale789092 <valeska2110@estudiantec.cr>
2022-03-11 18:54:58 -0600

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,38 @@
+public class Circulo extends Figura {
+    private double radio;
+    
+    public Circulo(double pradio){
+        radio = pradio;
+    }
+
+    //implementacion del metodo abstracto
+    public double calcularArea(){
+        double pi = Math.PI;
+        double radioele = Math.pow(radio, 2);
+        return pi * radioele;
+    }
+
+    public int ladosFigura(){
+        return 1;
+    }
+
+    //implementacion del metodo compareTo
+    public int compareTo(Circulo rectPorComparar){
+        double areaPorComparar = rectPorComparar.calcularArea();
+        int ladosPorComparar = rectPorComparar.ladosFigura();
+        double miArea = this.calcularArea();
+        int misLados = this.ladosFigura();
+        if(miArea == areaPorComparar){
+            if(misLados == ladosPorComparar)
+                return 1;
+            else if(misLados > ladosPorComparar)
+                return 0;
+            else
+                return -1;
+        }
+        else if (miArea > areaPorComparar)
+            return 0;
+        else
+            return -1;
+    }
+}
