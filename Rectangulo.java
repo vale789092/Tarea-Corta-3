22a08273812ca1216fbc2b95f2d56dff644f101a
vale789092 <valeska2110@estudiantec.cr>
2022-03-16 13:37:49 -0600

    Update Rectangulo.java

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,5 +1,5 @@
  //figura es bastract porque tiene al menos un metodo abstracto
- public class Rectangulo extends Figura implements Comparable<Rectangulo>{
+ public class Rectangulo extends Figura implements Comparable<Figura>{
     private double base;
     private double altura;
     
@@ -18,21 +18,21 @@
     }
 
     //implementacion del metodo compareTo
-    public int compareTo(Rectangulo rectPorComparar){
+    public int compareTo(Figura rectPorComparar){
         double areaPorComparar = rectPorComparar.calcularArea();
         int ladosPorComparar = rectPorComparar.ladosFigura();
         double miArea = this.calcularArea();
         int misLados = this.ladosFigura();
         if(miArea == areaPorComparar){
             if(misLados == ladosPorComparar)
-                return 1;
-            else if(misLados > ladosPorComparar)
                 return 0;
+            else if(misLados > ladosPorComparar)
+                return 1;
             else
                 return -1;
         }
         else if (miArea > areaPorComparar)
-            return 0;
+            return 1;
         else
             return -1;
     }
